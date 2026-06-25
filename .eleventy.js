8c6c8e04aa71e2b2bb724f0ab128a7762ee8fa6a
kimmie-p <kimberly@e-rehab.com>
2026-06-25 03:56:10 -0700

    Fix workflow path

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,14 @@
 module.exports = function(eleventyConfig) {
-  // Copy static assets straight through
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("images");
 
   return {
+    htmlTemplateEngine: "liquid",
     dir: {
       input: ".",
       output: "_site",
       includes: "_includes",
-      layouts: "_layouts"
     }
   };
 };
